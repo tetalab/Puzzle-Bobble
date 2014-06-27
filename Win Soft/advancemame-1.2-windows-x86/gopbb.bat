ed1143f5bfcd19d9b0fa1f03543a418461a9dc8a
ScAnim-Lionel <lionel.delteil@science-animation.org>
2014-06-27 15:45:21 +0200

    Windows Software Add
    Windows Software Add
    - GlovePie & Script
    - AdvanceMame
    - PPJoy

---
+++ Win Soft/advancemame-1.2-windows-x86/gopbb.bat
@@ -0,0 +1,1 @@
+C:\Users\Lionel\Downloads\taitoAkbar\GlovePIE045Free\PIEFree -C:\Users\Lionel\Downloads\taitoAkbar\GlovePIE045Free\MouseJoystick.PIE "/r:startgame.bat"
---
+++ Win Soft/advancemame-1.2-windows-x86/startgame.bat
@@ -0,0 +1,1 @@
+C:\Users\Lionel\Downloads\taitoAkbar\advancemame-1.2-windows-x86\advmame pbobble
